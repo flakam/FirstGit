c289789e18943dc524469fc1ecbbbbcc17e8f931
Flaka Mahmuti <flakamahmuti@gmail.com>
2019-07-09 21:33:27 -0400

    First commit

---
+++ FirstGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello GITHUB!");
+        }
+    }
+}
